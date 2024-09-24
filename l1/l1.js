fb614a7515b5cc02152c690b9cee4e1703259c45
Emanuele Tocci <emato03.felix@gmail.com>
2024-09-24 18:11:28 +0200

    update

--- l1/l1.js
+++ l1/l1.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const modal = document.querySelector('.modal');
     const main = document.getElementById("main-section");
     const close_button = document.getElementById('close-button');
-    const modalct = document.getElementById('modal-ct');
+    const modalct = document.getElementById('modal-ct'); 
 
     let start_time;
     let counter;
@@ -23,12 +23,15 @@ document.addEventListener('DOMContentLoaded', function(){
         clearTimeout(intIdModal);
     }
     intIdModal = setTimeout( function(){ 
-        modal.classList.remove("hidden");
+        modalct.classList.remove("hidden");
+        modalct.classList.add("bg-blur");
     }, 500);  //open dialog after 500ms
 
     close_button.addEventListener('click', function(){
         modal.classList.add("hidden");  //close dialog on click
         modalct.style.zIndex="-1";
+        modalct.classList.remove("bg-blur");
+
     });
     container.addEventListener('click', clicked);
     reset.addEventListener('click', reset_func);
